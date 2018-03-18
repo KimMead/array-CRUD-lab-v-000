bde348388f5d0f6b633ca5315461717983fe9e25
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-03-18 23:43:53 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,9 +9,10 @@ end
 
 def add_element_to_end_of_array(array, element)
   create_an_array.push("arrays!")
+  add_element_to_end_of_array(-1, "arrays!")
 end
 
-add_element_to_end_of_array(-1, "arrays!")
+
 
 def add_element_to_start_of_array(array, element)
   create_an_array.unshift("wow")
