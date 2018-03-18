c533a0e920eb03f28efc8d3dda8690da2b34807f
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-03-18 23:50:42 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,7 +11,6 @@ def add_element_to_end_of_array(array, element)
   create_an_array.push("arrays!")
 end
 
-
 def add_element_to_start_of_array(array, element)
   create_an_array.unshift("wow")
 end
@@ -25,9 +24,10 @@ end
 remove_element_from_end_of_array(-1)
 
 def remove_element_from_start_of_array(array)
-  create_an_array.shift
+  create_an_array.shift("wow")
 end
 
+remove_element_from_start_of_array(0)
 
 def retrieve_element_from_index(array, index_number)
   create_an_array[index_number]
