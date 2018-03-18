7581c372b87dde6246c7d2695ffa82a83a3f7ec7
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-03-18 20:53:48 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,13 +23,13 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  create_an_array.first
+  create_an_array[]
 end
 
 def retrieve_first_element_from_array(array)
-
+  create_an_array.first
 end
 
 def retrieve_last_element_from_array(array)
-
+  create_an_array.last
 end
