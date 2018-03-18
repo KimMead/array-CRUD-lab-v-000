503216b00bffbfd8badc5a18f67e648ac8af4ba2
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-03-18 20:33:12 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,7 +11,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  animals.unshift ("pony")
+  create_an_array.unshift ("pony")
 end
 
 def remove_element_from_end_of_array(array)
