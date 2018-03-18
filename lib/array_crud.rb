3270fd7b65131f11266e990c61e3d1a5f0ec5c14
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-03-18 20:26:11 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,11 +7,11 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  create_an_array << ["horsey"]
+  create_an_array.push("horsey")
 end
 
 def add_element_to_start_of_array(array, element)
-  create_an_array.unshift ("pony")
+  create_an_array.unshift("pony")
 end
 
 def remove_element_from_end_of_array(array)
