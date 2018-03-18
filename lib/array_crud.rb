6b16f9089155b8079a11ef979ac277806de5d80e
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-03-18 20:18:37 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["puppy, "kitty", "fishy", "birdy"]
+  ["puppy", "kitty", "fishy", "birdy"]
 end
 
 def add_element_to_end_of_array(array, element)
